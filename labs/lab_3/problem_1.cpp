6bfe39785976f60563011a90bff2c955d7555270
Korn <kornkubzaza@gmail.com>
2021-08-25 01:22:20 +0700

    กำลังทำ Lab 2

---
+++ labs/lab_3/problem_1.cpp
@@ -0,0 +1,27 @@
+/*
+    Problem : ให้เขียนการทํางานของการหาผลบวกของตัวเลขโดดที่ไม่ใช่ 0 ทุกตัวในตัวเลขที่ป้อน ไปเรื่อยๆ จนกว่าจะเป็นเลขหลักเดียวเช่นเมื่อป้อน input เป็น 123456 จะได้ผลลัพธ์คือ 123456 -> 21-> 3
+    Author : กร โรจน์รัตนปัญญา (64010009)
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    string number;
+    cin >> number;
+
+    while(number.size() > 1) {
+
+        int number_added = 0;
+        for(int i = 0; i < number.size(); i++) {
+
+            number_added += (int)number.at(i) - 48;
+        }
+        number = to_string(number_added);
+    }
+    cout << number.at(0) << endl;
+
+    return 0;
+}
