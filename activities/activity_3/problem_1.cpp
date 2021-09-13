61d07f7a011db6a0033cb90b3a9e0e35b8ebe596
Korn <kornkubzaza@gmail.com>
2021-09-14 03:03:09 +0700

    ืทำ Activity #4 เสร็จ

---
+++ activities/activity_3/problem_1.cpp
@@ -0,0 +1,31 @@
+/*
+    Problem : ตรวจว่าเป็นเลขจำนวนเฉพาะหรือไม่
+    Author : กร โรจน์รัตนปัญญา (64010009)
+*/
+
+#include <iostream>
+using namespace std;
+
+bool isPrime(int number) {
+
+    if(number == 1) return false;
+    for(int i = number/2; i > 1; i--) {
+        if(number % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+
+    cout << "Enter number : ";
+
+    int number;
+    cin >> number;
+
+    for(int i = 0; i < number; i++) {
+        if(isPrime(number)) cout << number << " is a prime number." << endl;
+        else cout << number << " is not a prime number." << endl;
+    }
+
+    return 0;
+}
