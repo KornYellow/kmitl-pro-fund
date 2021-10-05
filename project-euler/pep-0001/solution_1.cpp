e1c20667ef88126e9c1703247d40a70774491e1c
Korn <kornkubzaza@gmail.com>
2021-10-05 19:08:25 +0700

    Project Euler ข้อ #1 เสร็จ

---
+++ project-euler/pep-0001/solution_1.cpp
@@ -0,0 +1,20 @@
+/*
+    Level : 1
+    Author : กร โรจน์รัตนปัญญา (64010009)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int result = 0;
+    for(int i = 3; i < 1000; i++) {
+
+        if(i % 3 == 0) result += i;
+        else if(i % 5 == 0) result += i;
+    }
+    cout << result << endl;
+
+    return 0;
+}
