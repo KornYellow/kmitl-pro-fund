7605b33fb5b8ade6b24d8753bc6af13eb86a94b7
Korn <kornkubzaza@gmail.com>
2021-10-05 20:03:53 +0700

    เพิ่ม Solution ที่ 2

---
+++ project-euler/pep-0003/solution_2.cpp
@@ -0,0 +1,25 @@
+/*
+    Level : 3
+    Author : กร โรจน์รัตนปัญญา (64010009)
+*/
+
+#include <iostream>
+using namespace std;
+
+long int largestPrimeFactor(long int number) {
+
+    long int divisor = 2;
+    while(number > 1) {
+
+        if(number % divisor == 0) number = number / divisor;
+        else divisor ++;
+    }
+    return divisor;
+}
+
+int main() {
+
+    cout << largestPrimeFactor(600851475143) << endl;
+    
+    return 0;
+}
