58228cca0b1264de9edfbae064818cbaf78a5f61
Korn <kornkubzaza@gmail.com>
2021-10-05 19:22:43 +0700

    Project Euler ข้อที่ #2 เสร็จ

---
+++ project-euler/pep-0002/solution_1.cpp
@@ -0,0 +1,27 @@
+/*
+    Level : 1
+    Author : กร โรจน์รัตนปัญญา (64010009)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int term_first = 1;
+    int term_second = 2;
+
+    int result = 0;
+    while(term_second < 4000000) {
+
+        if(term_second % 2 == 0) result += term_second;
+
+        int temp = term_second;
+        term_second = term_first + term_second;
+        term_first = temp;
+    }
+
+    cout << result << endl;
+
+    return 0;
+}
