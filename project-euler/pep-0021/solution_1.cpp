16b7faa0b580f2da3b39acfe336835d9b999ac75
Korn <kornkubzaza@gmail.com>
2021-10-20 01:29:50 +0700

    Project Euler ข้อที่ #21

---
+++ project-euler/pep-0021/solution_1.cpp
@@ -0,0 +1,34 @@
+/*
+    Problem :
+    Level :
+    Author : กร โรจน์รัตนปัญญา (64010009)
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int sumOfDivisor(int number) {
+
+    int sum = 0;
+    for(int i = 1; i <= number / 2; i++) {
+
+        if(number % i == 0) sum += i;
+    }
+
+    return sum;
+}
+
+int main() {
+
+    int result = 0;
+    for(int i = 1; i < 10000; i++) {
+
+        int sum = sumOfDivisor(i);
+        if(i == sumOfDivisor(sum) && i != sum) result += i;
+    }
+
+    cout << result << endl;
+
+    return 0;
+}
